49d34bfe2e8c63510de7c1c0875ee2c64eeb9bec
Liyang Wang (C) <liyangw@splunk.com>
2021-04-16 16:32:02 -0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 cd /pink-panther
-python3 run_json.py --mode=check --path=${GITHUB_WORKSPACE}
+python3 run_json.py \
+--mode=check \
+--path=${GITHUB_WORKSPACE} \
+--url=${GITHUB_REPOSITORY} \
+--excluded_dirs_path=.excluded_dirs \
+--excluded_files_path=.excluded_files \
+--err_file=err_biased_lang.log \
+--splunk_logs
