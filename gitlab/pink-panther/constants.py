ec40a99553b93f8fde7a0af7b5423c899f2f0f31
Liyang Wang (C) <liyangw@splunk.com>
2021-05-25 14:36:47 -0700

    remove gitlab repo

--- gitlab/pink-panther/constants.py
+++
@@ -1,5 +0,0 @@
-SUMMARY_FILENAME = 'biased-language-summary.json'
-CODECLIMATE_FILENAME = 'biased-language.codeclimate.json'
-BIASED_WORDS_FILE = 'word_list.csv'
-LOG_FILE = 'biased_language.log'
-MAX_LINE_LEN = 150
--- gitlab/pink-panther/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-from .utils import get_splunk_hec_info, get_colors, get_colors_sh, get_batch_info
-from .utils import truncate_line, get_source_type, send_codeclimate_batch, open_csv
-from .utils import exclude_pink_panther_dirs_files
-from .utils import exclude_other_dirs_files, write_file, grab_repo_name
-from .utils import TimeFunction, BiasedLanguageLogger, get_line_count
